99b166ff80410591ce025d30bed4f695bfa5a188
tatyabicchu <vishal.kumar-ug23@nsut.ac.in>
2024-10-16 22:06:39 +0530

    cumcoins1st

--- src/router/index.js
+++ src/router/index.js
@@ -46,7 +46,7 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/login.vue')
+      component: () => import('../views/Login.vue')
     },
     {
       path: '/signup',
